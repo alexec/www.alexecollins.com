d2e7468bec5545cd7729161a1ca0f6284fb1cb22
alex.collins <alex.collins@gamesys.co.uk>
2015-12-30 21:34:59 +0000

    updated upload

--- upload.sh
+++ upload.sh
@@ -1,6 +1,23 @@
 #! /bin/sh
-set -eux
+set -eu
 
-gsutil -m rsync -R build gs://www.alexecollins.com
+cd build
+
+if [ $(file js/jquery.js | grep -c gzip || echo) = 0 ]; then
+  echo "gzipping"
+  for F in $(find . -path '*.css' -or -path '*.html' -or -path '*.js'); do
+      cat $F | gzip > tmp
+      mv tmp $F
+  done
+fi
+
+find . -name tmp | xargs rm -
+
+gsutil web set -m index.html -e 404.html gs://www.alexecollins.com
+gsutil -m rsync -R . gs://www.alexecollins.com
 gsutil -m acl ch -u AllUsers:r -r gs://www.alexecollins.com/
 
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -r gs://www.alexecollins.com/
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/css" -r gs://www.alexecollins.com/**.css
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/html" -r gs://www.alexecollins.com/**.html
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/javascript" -r gs://www.alexecollins.com/**.js
