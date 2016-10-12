b1a0a103aaa87144766eef73cf269b1118ceb2fd
Alex Collins <alex.e.c@gmail.com>
2016-10-12 20:56:46 +0100

    set 404 page

--- upload.sh
+++ upload.sh
@@ -16,6 +16,7 @@ gsutil web set -m index.html -e 404.html gs://www.alexecollins.com
 gsutil -m rsync -R . gs://www.alexecollins.com
 gsutil -m acl ch -u AllUsers:r -r gs://www.alexecollins.com/
 
+gsutil web set -m index.html -e 404/index.html gs://www.alexecollins.com/
 gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -r gs://www.alexecollins.com/
 gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/css" -r gs://www.alexecollins.com/**.css
 gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/html" -r gs://www.alexecollins.com/**.html
