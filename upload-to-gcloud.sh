b6805135845a650a5bf9fd989fc3cd806448cd8d
Alex Collins <alex.e.c@gmail.com>
2017-01-22 10:13:30 +0000

    added upload script

---
+++ upload-to-gcloud.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+set -eu
+
+rm -Rf upload
+cp -R build upload
+cd upload
+
+for F in $(find . -path '*.css' -or -path '*.html' -or -path '*.js'); do
+    cat $F | gzip > tmp
+    mv tmp $F
+done
+
+find . -name tmp | xargs rm -
+
+gsutil web set -m index.html -e 404.html gs://www.alexecollins.com
+gsutil -m rsync -R . gs://www.alexecollins.com
+gsutil -m acl ch -u AllUsers:r -r gs://www.alexecollins.com/
+
+gsutil web set -m index.html -e 404/index.html gs://www.alexecollins.com/
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -r gs://www.alexecollins.com/
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/css" -r gs://www.alexecollins.com/**.css
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/html" -r gs://www.alexecollins.com/**.html
+gsutil -m setmeta -h "Cache-Control:public, max-age=604800" -h "Content-Encoding:gzip" -h "Content-Type:text/javascript" -r gs://www.alexecollins.com/**.js
